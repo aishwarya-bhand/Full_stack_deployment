3dcce476f5d31e184f96094f8ab553bb8cb65699
Aishwarya Bhand <aishwaryabhand.devops@gmail.com>
2026-03-03 21:36:18 +0530

    Initial full stack dockerized app

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello from Backend 🚀",
+    environment: process.env.NODE_ENV || "development"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
